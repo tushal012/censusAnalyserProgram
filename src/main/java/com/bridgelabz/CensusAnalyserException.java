9c4a64fcea8de76f643bc16bad04b242fe0f5204
tushal kumar <tushalkashyap11@gmail.com>
2021-06-17 19:51:01 -0700

    Added: Test case to check CSV file path is incorrect

--- src/main/java/com/bridgelabz/CensusAnalyserException.java
+++ src/main/java/com/bridgelabz/CensusAnalyserException.java
@@ -1,11 +1,13 @@
 package com.bridgelabz;
 
 public class CensusAnalyserException extends Exception {
-    enum ExceptionType {
+    public ExceptionType exceptionType;
+
+    public enum ExceptionType {
         UNABLE_TO_PARSE, CENSUS_FILE_PROBLEM, HEADERS_INVALID, NOT_A_CSV_TYPE;
     }
 
-    ExceptionType exceptionType;
+
 
     public CensusAnalyserException(String message, ExceptionType exceptionType) {
         super(message);
--- src/test/java/CensusAnalyserTest.java
+++ src/test/java/CensusAnalyserTest.java
@@ -2,6 +2,7 @@ import com.bridgelabz.CensusAnalyser;
 import com.bridgelabz.CensusAnalyserException;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndianStateCensusData.csv";
@@ -17,5 +18,17 @@ public class CensusAnalyserTest {
             int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             Assert.assertEquals(29,numOfRecords);
         } catch (CensusAnalyserException e) { }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.exceptionType);
+        }
     }
 }
